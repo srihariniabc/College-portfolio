f9bfcb99f612ea354b3938191b40946d75d75959
srihariniabc <148522312+srihariniabc@users.noreply.github.com>
2023-10-20 14:20:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function revealMessage() {
+
+document.getElementById("hiddenMessage").style.display='block';
+}
+function revealcontact() {
+
+    document.getElementById("hidecontact").style.display='block';
+    }
